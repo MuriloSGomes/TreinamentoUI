7d0a547df109a16918ba25fdab0430494c18fc4c
MuriloSGomes <seno.murilo@hotmail.com>
2019-08-14 00:00:56 -0300

    Adiciona novas dependêcias de font Implementa input de login e senha Altera cores do gradient e tambem adiciona novas no botao login

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -10,37 +10,41 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          stops: [0.1, 0.46],
+          colors: [Color(0xFF385ff1), Color(0xFF7414d8)],
+        )),
         child: ListView(
           children: <Widget>[
             Container(
-              height: 350,
+              height: 300,
               width: MediaQuery.of(context).size.width,
               child: ClipPath(
                 clipper: HeaderColor(),
                 child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Color(0xFFFCE4EC), Color(0xFFFCE4EC)],
-                      begin: Alignment.centerRight,
-                      end: Alignment(0, -1),
-                    ),
-                  ),
+                  color: Colors.white,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Align(
                         alignment: Alignment.center,
                         child: Image(
-                          height: 200,
+                          height: 170,
                           image: AssetImage('lib/assets/flutterlogo.png'),
                         ),
                       ),
                       Align(
                         alignment: Alignment.center,
-                        heightFactor: 3,
+                        heightFactor: 1.5,
                         child: Text(
-                          'SMARTIE',
-                          style: TextStyle(color: Colors.white, fontSize: 30),
+                          'smartie',
+                          style: TextStyle(
+                              fontFamily: 'Comfortaa',
+                              color: Colors.grey,
+                              fontSize: 43),
                         ),
                       ),
                     ],
@@ -49,44 +53,85 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Color(0xFF4527A0), Color(0xFF7E57C2)],
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                  stops: [0.3, 1.0],
-                )
-              ),
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width,
-              child: ListView(
-                padding: EdgeInsets.all(50),
+              child: Column(
                 children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width / 1.2,
-                    height: 45,
-                    padding:
-                        EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(color: Colors.black12, blurRadius: 5)
-                        ]),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        icon: Icon(
-                          Icons.person,
-                          color: Colors.grey,
-                        ),
-                        hintText: 'Email or Username',
-                      ),
+                  Align(
+                    alignment: Alignment.center,
+                    heightFactor: 2.4,
+                    child: Text(
+                      'Hi There!',
+                      style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          color: Colors.white,
+                          fontSize: 16),
                     ),
                   ),
+                  Align(
+                    heightFactor: 0,
+                    child: Text(
+                      'Login to Continue',
+                      style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          color: Colors.white,
+                          fontSize: 16),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: "Email or Username",
+                      labelStyle: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          color: Colors.grey,
+                          fontSize: 18),
+                      suffixIcon: Icon(Icons.person_outline),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 18,
+                        ),
+                        suffixIcon: Icon(Icons.lock_outline)),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width/1.2,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Color(0xFFf45d27), Color(0xFFf581f)]
+                      ),
+                      borderRadius: BorderRadius.all(Radius.circular(50))
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontFamily: 'Comfortaa',
+                          color: Colors.white,
+                          fontSize: 15
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
-            ),
+            )
           ],
         ),
       ),

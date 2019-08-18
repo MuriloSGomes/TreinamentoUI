0319265a31b2d42320c46aeb8146dbdfc918aea7
MuriloSGomes <seno.murilo@hotmail.com>
2019-08-18 11:23:30 -0300

    adiciona plugin de linha tracejada e img

--- lib/roompage.dart
+++ lib/roompage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dashed_container/dashed_container.dart';
 
 class RoomPage extends StatefulWidget {
   @override
@@ -9,30 +10,59 @@ class _RoomPageState extends State<RoomPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 10,
-        backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.add_circle_outline,
-              color: Colors.blueAccent,
-              size: 40,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(95.0),
+        child: AppBar(
+          elevation: 10,
+          backgroundColor: Colors.white,
+          title: Align(
+            alignment: Alignment(0.0, 0.7),
+            child: Text(
+              'Rooms',
+              style: TextStyle(
+                fontFamily: 'Comfortaa-Regular',
+                color: Colors.black,
+                fontSize: 20,
+              ),
             ),
-            onPressed: () {},
-            padding: EdgeInsets.only(right: 12),
           ),
-        ],
-        title: Text(
-          'Rooms',
-          style: TextStyle(
-            fontFamily: 'Comfortaa-Regular',
-            color: Colors.black,
-            fontSize: 20
+          centerTitle: true,
+          actions: <Widget>[
+            Align(
+              alignment: Alignment(0, 1),
+              child: IconButton(
+                icon: Icon(Icons.add_circle_outline,
+                    color: Colors.blueAccent, size: 40),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              DashedContainer(
+                child: Container(
+                  height: 200.0,
+                  width: 400.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('lib/assets/cozinha.jpg'),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
-        centerTitle: true,
       ),
     );
+  }
+
+  Widget EscpacoEntreBox() {
+    return SizedBox(height: 40.0);
   }
 }

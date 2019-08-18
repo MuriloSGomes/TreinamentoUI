76cfda01ae96bdeb48624713538392deeb6348ef
MuriloSGomes <seno.murilo@hotmail.com>
2019-08-18 13:07:15 -0300

    Retira imgs do arquivo que não vão ser mais uteis Adiciona nova imagem, plugin para fazer a caixa de uma linha tracejada adiciona navigation button itens

--- lib/roompage.dart
+++ lib/roompage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dashed_container/dashed_container.dart';
 
@@ -29,7 +30,7 @@ class _RoomPageState extends State<RoomPage> {
           centerTitle: true,
           actions: <Widget>[
             Align(
-              alignment: Alignment(0, 1),
+              alignment: Alignment(0, 1.9),
               child: IconButton(
                 icon: Icon(Icons.add_circle_outline,
                     color: Colors.blueAccent, size: 40),
@@ -39,30 +40,102 @@ class _RoomPageState extends State<RoomPage> {
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              DashedContainer(
-                child: Container(
-                  height: 200.0,
-                  width: 400.0,
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 230,
+                  width: 350,
+                  alignment: Alignment.bottomLeft,
+                  padding: EdgeInsets.only(left: 10.0, bottom: 8.0, top: 200),
                   decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('lib/assets/cozinha.jpg'),
+                      image: NetworkImage(
+                          "https://http2.mlstatic.com/cozinha-completa-balco-com-tampo-5-pecas-carrara-i-hd-D_NQ_NP_695102-MLB27156797377_042018-F.jpg"),
+                      fit: BoxFit.cover,
                     ),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Kitchen',
+                        style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontSize: 20,
+                            color: Colors.white),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Text(
+                          '3 Devices',
+                          style: TextStyle(
+                              fontFamily: 'Comfortaa',
+                              fontSize: 20,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              )
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                DashedContainer(
+                  child: Container(
+                    height: 230,
+                    width: 350,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '+ Add room',
+                        style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontSize: 13,
+                            color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                  dashColor: Colors.grey,
+                  borderRadius: 20.0,
+                  dashedLength: 7,
+                  strokeWidth: 2,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text(''),
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.notifications),
+            title: new Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.donut_small),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text(''),
+          ),
+        ],
       ),
     );
-  }
-
-  Widget EscpacoEntreBox() {
-    return SizedBox(height: 40.0);
   }
 }

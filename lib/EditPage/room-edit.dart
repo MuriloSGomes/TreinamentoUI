326fc62b8c75beec5ed8dfdde4e23d156e8650b0
MuriloSGomes <seno.murilo@hotmail.com>
2019-08-22 20:53:13 -0300

    - Adiciona novas imagens - Alinha imagens com o box decoration - Cria switch button

--- lib/EditPage/room-edit.dart
+++ lib/EditPage/room-edit.dart
@@ -9,6 +9,7 @@ class RoomEdit extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(170.0),
         child: Stack(
@@ -68,49 +69,121 @@ class RoomEdit extends StatelessWidget {
           Align(
             alignment: Alignment(-0.9, -0.9),
             child: Container(
+              alignment: Alignment(0, 0.6),
+              child: Switch(
+                inactiveThumbColor: Colors.white,
+                inactiveTrackColor: Colors.orange,
+                value: true,
+              ),
               height: 170,
               width: 170,
               decoration: BoxDecoration(
-                color: Colors.white70,
+                image: DecorationImage(
+                  alignment: Alignment(0, -2.2),
+                  image: AssetImage('lib/assets/ar.png'),
+                ),
+                color: Colors.white,
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(20),
               ),
             ),
           ),
           Align(
+            alignment: Alignment(-0.56, -0.57),
+            child: Text(
+              'A/C',
+              style:
+                  TextStyle(fontFamily: 'Comfortaa', color: Colors.blueAccent),
+            ),
+          ),
+          Align(
             alignment: Alignment(0.9, -0.9),
             child: Container(
+              alignment: Alignment(0, 0.65),
+              child: Switch(
+                inactiveThumbColor: Colors.white,
+                inactiveTrackColor: Colors.orange,
+                value: true,
+              ),
               height: 170,
               width: 170,
               decoration: BoxDecoration(
-                color: Colors.white70,
+                image: DecorationImage(
+                  alignment: Alignment(0,-1),
+                  image: AssetImage('lib/assets/lampada.png')
+                ),
+                color: Colors.white,
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(-0.93, -0.34),
+            child: SizedBox(
+              height: 35,
+              child: Image(
+                image: AssetImage('lib/assets/ice.png'),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(-0.75, -0.32),
+            child: Text(
+              '25°',
+              style: TextStyle(fontFamily: 'Comfortaa', color: Colors.grey, fontSize: 15),
+            ),
+          ),
+          Align(
+            alignment: Alignment(-0.23, -0.34),
+            child: SizedBox(
+              height: 25,
+              child: Image(
+                image: AssetImage('lib/assets/settings.png'),
               ),
             ),
           ),
           Align(
             alignment: Alignment(-0.9, 0.5),
             child: Container(
+              alignment: Alignment(0, 0.6),
+              child: Switch(
+                inactiveThumbColor: Colors.white,
+                inactiveTrackColor: Colors.orange,
+                value: true,
+              ),
               height: 170,
               width: 170,
               decoration: BoxDecoration(
-                color: Colors.white70,
+                color: Colors.white,
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(20),
               ),
             ),
           ),
           Align(
+            alignment: Alignment(-0.72, 0.17),
+            child: SizedBox(
+              height: 80,
+              child: Image(
+                image: AssetImage('lib/assets/camera.png'),
+              ),
+            ),
+          ),
+          Align(
             alignment: Alignment(0.9, 0.5),
             child: Container(
+              alignment: Alignment(0, 0.4),
+              child: Switch(
+                inactiveThumbColor: Colors.white,
+                inactiveTrackColor: Colors.grey,
+                value: false,
+              ),
               height: 170,
               width: 170,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('')
-                ),
-                color: Colors.white70,
+                image: DecorationImage(image: AssetImage('')),
+                color: Colors.white,
                 shape: BoxShape.rectangle,
                 borderRadius: BorderRadius.circular(20),
               ),
